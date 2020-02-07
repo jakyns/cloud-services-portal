8b279552035e9854d1ad0c5223bb1ac4c397d768
bank <jak.yns@gmail.com>
2020-02-07 16:32:10 +0700

    Apply request class to provider class

--- api/storage/storage/gcp/provider.py
+++ api/storage/storage/gcp/provider.py
@@ -1,10 +1,7 @@
-from google.cloud import storage
-from google.cloud import exceptions
-
 from api.storage.storage.base.provider import Provider as BaseProvider
-from api.storage.storage.gcp.response import Response as GCPResponse
 
-from lib.storage import errors as StorageError
+from api.storage.storage.gcp.request import Request as GCPRequest
+from api.storage.storage.gcp.response import Response as GCPResponse
 
 
 class Provider(BaseProvider):
@@ -13,9 +10,14 @@ class Provider(BaseProvider):
     def set_bucket(self, bucket) -> None:
         self.bucket = bucket
 
+    def get_bucket(self) -> str:
+        return self.bucket
+
     def request_upload(self, remote_file_path, local_file_path) -> dict:
-        file_object = self.__upload(remote_file_path, local_file_path)
-        response = self.__build_storage_response(file_object)
+        request = GCPRequest(self.bucket)
+        upload_response = request.upload(remote_file_path, local_file_path)
+
+        response = self.__build_storage_response(upload_response)
 
         return {
             "id": response.id(),
@@ -26,8 +28,10 @@ class Provider(BaseProvider):
         }
 
     def request_delete(self, remote_file_path) -> dict:
-        file_object = self.__delete(remote_file_path)
-        response = self.__build_storage_response(file_object)
+        request = GCPRequest(self.bucket)
+        delete_response = request.delete(remote_file_path)
+
+        response = self.__build_storage_response(delete_response)
 
         return {
             "id": response.id(),
@@ -38,34 +42,6 @@ class Provider(BaseProvider):
         }
 
     # private
-
-    def __storage_client(self) -> storage.blob:
-        return storage.Client().get_bucket(self.bucket)
-
-    def __blob_object(self, remote_file_path) -> storage.blob.Blob:
-        try:
-            storage_client = self.__storage_client()
-            blob = storage_client.blob(remote_file_path)
-
-            return blob
-        except exceptions.NotFound as e:
-            raise StorageError.BucketNotFound
-
-    def __upload(self, remote_file_path, local_file_path) -> storage.blob.Blob:
-        blob = self.__blob_object(remote_file_path)
-        blob.upload_from_filename(local_file_path)
-
-        return blob
-
-    def __delete(self, remote_file_path) -> storage.blob.Blob:
-        blob = self.__blob_object(remote_file_path)
-
-        if not blob.exists():
-            raise StorageError.FileNotFound
-
-        blob.delete()
-
-        return blob
 
     def __build_storage_response(self, response) -> GCPResponse:
         return GCPResponse(response)
